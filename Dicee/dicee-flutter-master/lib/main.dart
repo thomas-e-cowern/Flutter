3159220cee750bbffb8a740b8e7fcc31be2f0258
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-01-19 11:01:41 -0500

    centered dice in view

--- Dicee/dicee-flutter-master/lib/main.dart
+++ Dicee/dicee-flutter-master/lib/main.dart
@@ -18,15 +18,17 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset('images/dice1.png')
-        ),
-        Expanded(
-          child: Image.asset('images/dice2.png')
-        )
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Image.asset('images/dice1.png')
+          ),
+          Expanded(
+            child: Image.asset('images/dice2.png')
+          )
+        ],
+      ),
     );
   }
 }
