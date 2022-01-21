b66cb715049cef52351806baf4d813f979b4fe74
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-01-21 08:30:23 -0500

    added random function

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -34,12 +35,16 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+
+  int ballNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextButton(
         onPressed: () {
-          print('Ball Pressed');
+          ballNumber = Random().nextInt(4) + 1;
+          print('ballNumber is $ballNumber');
         },
         child: Image.asset('images/ball1.png'),
       ),
